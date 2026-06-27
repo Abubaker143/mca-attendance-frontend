0a7da3bdaec577524a8c9358738f2027de39ac3e
abu-the-developer <147492621+Abubaker143@users.noreply.github.com>
2026-06-27 17:42:29 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AttendanceDashboard from './components/AttendanceDashboard';
+
+function App() {
+  return (
+    <div style={{ minHeight: '100vh', background: '#f9fafb', padding: '10px' }}>
+      <AttendanceDashboard />
+    </div>
+  );
+}
+
+export default App;
